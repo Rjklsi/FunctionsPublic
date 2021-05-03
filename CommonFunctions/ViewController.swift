e578332efc0c71291c53f98eb79d8795b9eb2c25
Mitesh Khatri <miteshkhat@gmail.com>
2021-05-03 15:20:57 +0530

    Initial commit

---
+++ CommonFunctions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CommonFunctions
+//
+//  Created by mac on 03/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
